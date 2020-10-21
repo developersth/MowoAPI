a0273a62372720f9e220537ac346c436ca731ca0
developersth <kritsadee.satewin@gmail.com>
2020-10-21 23:39:11 +0700

    crate hospital

--- controllers/hospital.controler.js
+++ controllers/hospital.controler.js
@@ -5,6 +5,41 @@ const jwt = require('jsonwebtoken')
 // define variable
 const Hospital = db.hospital;
 const { Op } = require("sequelize");//condition
+exports.create = async function (req, res) {
+    try {
+        const { hospital_name, address, tel, lat,lng,status} = req.body;
+        var check_data_exist = Hospital.findAll({
+            where: {
+                [Op.or]: [
+                    { hospital_name: hospital_name },
+                ]
+            }
+        });
+        const newData = {
+            hospital_name:hospital_name,
+            address: address,
+            tel: lat,
+            lng: lng,
+            status: status,
+           
+        };
+         check_data_exist.then(function (newData) {
+            if (newData.length == 0) {
+                Hospital.create(newData).then(data => {
+                    res.send({ success: true, message: 'Hospital Created Successfully', data });
+                })
+                    .catch(err => {
+                        res.status(500).send({ success: false, message: err.message || "Some error occurred while creating the users." });
+                    });
+            }
+            else {
+                res.send({ success: false, message: 'Hospital Name already in Hospital' });
+            }
+        })
+    } catch (error) {
+        res.status(500).send({ success: false, message: error });
+    }
+}
 exports.findAll = async function (req, res, next) {
     const result = Hospital.findAll();
     await result.then(function (data) {
--- routes/hospital.js
+++ routes/hospital.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const hospitalController = require('../controllers/hospital.controler');
 const db = require('../config/db.config');
 
+router.post('/', hospitalController.create);
 router.get('/', hospitalController.findAll);
 router.get('/:id', hospitalController.findOne);
 module.exports = router;
